5e5a82aacb3bcf8f0d9867e8e31cdf87b09eb5a1
Aurelien Bondis <abondis@pheromone.ca>
2015-12-22 16:55:34 -0500

    default log path

--- htpasswd_api/__init__.py
+++ htpasswd_api/__init__.py
@@ -17,7 +17,7 @@ IP = '127.0.0.1'
 DEBUG = os.environ.get('DEBUG', "1") == "1"
 # PASSWORDS_PATH = '/etc/nginx/passwords'
 PASSWORDS_PATH = os.environ.get('PASSWORDS_PATH', here() + '/passwords')
-LOGS = os.environ.get('LOGS', '{}/htpasswdapi.log'.format(here()))
+LOGS = os.environ.get('LOGS', '/var/log/htpasswd-api/htpasswdapi.log')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
